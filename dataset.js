459bd0cd1f601f40ef49427c044d741f0fdf2212
dmsgur <dmsgur@gmail.com>
2024-12-20 11:48:23 +0900

    완료

---
+++ dataset.js
@@ -0,0 +1,11 @@
+//데이터 17KPamLcwRpk7yZVpjMqqaumfIBL-8R8k
+data_sets = []
+//데이터 아키텍처{sub_title:"",sub_content:"",sub_img:[],user_fill:""}
+data_sets.push({sub_title:"이미지크롤링",
+  sub_content:"구글 사이트 및 네이버 사이트를 이동하면서 해당 검색어의 이미지를 자동 수집",
+  sub_img:["1S0rv5tVKoKnAqKRLrW7huVwCqFct3AnY","1pTmtTsFhCBLjsOIIDIBtx2MonQiwPc6h",
+    "1xjyNyS2sxtbov76bDhKaPzvucu-pGf58","1XXsGddarlNGJVYKwNl7c52eB_aup__9T"
+  ],
+  user_fill:"selenium 라이브러리와 크롬 드라이버를 활용하여 데이터 수집 자동화 - 관련없는 데이터는 임의 삭제 ",
+  asso_file:"관련파일 /Crawling/Craw_image_running 실행"
+})
