7d79af80d5a1c7c0354ac4184640f74d35fe6f36
codeOne <dmsgur@gmail.com>
2024-12-27 10:33:25 +0900

    Update common_menu.js

--- common_menu.js
+++ common_menu.js
@@ -13,8 +13,8 @@ menu1 = new Menu("CNN AND Crawling");
 menu1.url="https://dmsgur.github.io/classification_model/"
 menu1.tips="네이버,구글 이미지 크롤링 및 컨볼루션 적용한 모델";
 menu2 = new Menu("test menu")
-menu2.url = "127.0.0.1"
-menu2.tips = "테스트용 메뉴"
+menu2.url = "https://dmsgur.github.io/RNN_Encrypto/"
+menu2.tips = "가상화폐 분석을 이용한 미래 가격 측정"
 menu_sets.push(menu0)
 menu_sets.push(menu1)
 menu_sets.push(menu2)
